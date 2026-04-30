f16a00cd218cd1acc60712cd9db91410a034632a
lehlohonololuckymohapi007-bit <lehlohonololuckymohapi007@gmail.com>
2026-04-30 16:57:08 +0200

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,8 @@
+# MyFirstCode Challenge - Goodwall
+# Lehlonolo Lucky Mohapi - GitHub Certified 2026
+
+print("Hello World!")
+print("My name is Lehlonolo Lucky Mohapi") 
+print("I am from Sebokeng, South Africa")
+print("GitHub Certified Developer 2026")
+print("This is my #MyFirstCode submission")
